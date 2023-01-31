ae2b25787c2b7767bfb29f86f397a3eed8632380
DianaBedoya <dianabedoya570@gmail.com>
2023-01-31 09:16:45 -0500

    estructura inicial

---
+++ robot.js
@@ -0,0 +1,27 @@
+exports.createRobot(x, y) {
+  let x = "coordenada en x";
+  let y = "coordenada en y";
+  let orientation = "N" // dirección a la que apunta el robot "N", "E", "S", "W"
+
+  function advance() {
+    // función para avanzar
+  }
+
+  function turnRight() {
+    // función para girar a la derecha
+  }
+
+  function turnLeft() {
+    // función para girar a la izquierda
+  }
+
+  function instructions() {
+    // función para recibir una serie de instrucciones Ejm: "RAALAL"
+  }
+
+
+getPosition() {
+  // metodo para adquirir la posición
+}
+
+}
